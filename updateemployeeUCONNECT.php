5e9c3cf75b9e3169e5f2f4728abe85c042987bbd
Bhuvanmj <150059040+Bhuvanmj@users.noreply.github.com>
2026-01-06 01:55:22 +0530

    Update updateemployeeUCONNECT.php

--- updateemployeeUCONNECT.php
+++ updateemployeeUCONNECT.php
@@ -5,9 +5,6 @@ require_once "db.php";
 
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
