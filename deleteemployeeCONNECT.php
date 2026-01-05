0410b9ec78f8a6c1a6813c2ee65a834bacf18af7
Bhuvanmj <150059040+Bhuvanmj@users.noreply.github.com>
2026-01-06 01:51:23 +0530

    Update deleteemployeeCONNECT.php

--- deleteemployeeCONNECT.php
+++ deleteemployeeCONNECT.php
@@ -1,12 +1,7 @@
 <?php
 // Database connection
-$host     = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASS");
-$database = getenv("DB_NAME");
+require_once "db.php";
 
-
-$conn = new mysqli($host, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
