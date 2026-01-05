32ac21fb096dcabc9cccd5d63b1f502720fdfb9e
Bhuvanmj <150059040+Bhuvanmj@users.noreply.github.com>
2026-01-06 01:54:49 +0530

    Update addemployeeCONNECT.php

--- addemployeeCONNECT.php
+++ addemployeeCONNECT.php
@@ -4,9 +4,7 @@ require_once "db.php";
 
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
